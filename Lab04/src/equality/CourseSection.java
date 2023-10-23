a2caa6a947f5d045c39cdc890f7f209ad5811f31
noahgibony <noahgiboney@Noahs-MacBook-Air.local>
2023-10-23 11:41:41 -0700

    initial commit for lab04

---
+++ Lab04/src/equality/CourseSection.java
@@ -0,0 +1,24 @@
+package equality;
+
+import java.time.LocalTime;
+
+class CourseSection
+{
+   private final String prefix;
+   private final String number;
+   private final int enrollment;
+   private final LocalTime startTime;
+   private final LocalTime endTime;
+
+   public CourseSection(final String prefix, final String number,
+      final int enrollment, final LocalTime startTime, final LocalTime endTime)
+   {
+      this.prefix = prefix;
+      this.number = number;
+      this.enrollment = enrollment;
+      this.startTime = startTime;
+      this.endTime = endTime;
+   }
+
+   // additional likely methods not defined since they are not needed for testing
+}
