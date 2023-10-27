56801f53e5402821b1e059c9950d1e2cfe380e13
noahgibony <noahgiboney@Noahs-MacBook-Air.local>
2023-10-27 11:48:58 -0700

    tested lab

--- Lab04/src/equality/TestCases.java
+++ Lab04/src/equality/TestCases.java
@@ -4,10 +4,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.time.LocalTime;
 
 import org.junit.Test;
